4fb681cdba5b5d4b7a8294741a06678eec79a801
milenadeswart <milena.j.deswart@gmail.com>
2022-04-12 12:38:35 +0200

    1.1 almost done

--- intro2nlp_assignment1_code/intro2nlp_assignment1_code/TODO_analyses.py
+++ intro2nlp_assignment1_code/intro2nlp_assignment1_code/TODO_analyses.py
@@ -2,6 +2,7 @@
 # Run them on data/preprocessed/train/sentences.txt
 #from intro2nlp_assignment1_code.intro2nlp_assignment1_code.train import train
 import spacy
+import statistics
 
 # import dataset
 if __name__ == '__main__':
@@ -15,20 +16,27 @@ if __name__ == '__main__':
 ### TOKENIZATION ###
 nlp = spacy.load("en_core_web_sm")
 tokensize = 0
-typelist = {}
+typelist = []
+words = []
+no_words = []
 for line in train_sentences:
     line = nlp(line)
+    linelength = 0
     for token in line:
         tokensize+=1
-        if token in typelist.keys():
-            typelist[token] += 1
-        if token not in typelist:
-            typelist[token] = 1
+        if token.text not in typelist:
+            typelist.append(token.text)
+        # Let's filter out punctuation
+        if not token.is_punct:
+            words.append(token.text)
+            linelength +=1
         #print(token.text, token.pos_, token.dep_)
+    no_words.append(linelength)
 
 print("Token size:", tokensize) # 16130
-print("Type size:", typelist) # 16130
-#print(typelist)
+print("Type size:", len(typelist)) # 3746
+print("Number of words", len(words)) # 13895
+print("Average number of words", round(statistics.mean(no_words), 2)
 
 ### WORD CLASSES ###
 
