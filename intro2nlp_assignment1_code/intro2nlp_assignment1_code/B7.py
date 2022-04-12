ece819667886eecb06beb2411be51f414086ae1b
Arne <arne.sailer@gmx.net>
2022-04-12 17:07:57 +0200

    add task 8

--- intro2nlp_assignment1_code/intro2nlp_assignment1_code/B7.py
+++ intro2nlp_assignment1_code/intro2nlp_assignment1_code/B7.py
@@ -1,9 +1,9 @@
 import spacy
 import pandas as pd
 from wordfreq import word_frequency as wf
-data = pd.read_csv("data/original/english/WikiNews_Train.tsv", sep='\t', usecols=[4,9,10], header=None)
-data.columns = ["target","binary_label", "probabilistic label"]
-
+import matplotlib.pyplot as plt
+data = pd.read_csv("data/original/english/WikiNews_Train.tsv", sep='\t', usecols=[1,4,9,10], header=None)
+data.columns = ["sentence","target","binary_label", "probabilistic label"]
 print("binary_label")
 print(data["binary_label"].value_counts())
 
@@ -36,13 +36,32 @@ data2 = data[data.binary_label == 1]
 list = []
 list_c = []
 list_feq = []
+list_pos = []
+#get length of target words and word count of target
 for target in data2["target"]:
     target = nlp(target)
     list.append(len(target.doc))
     list_c.append(len(target.text))
 data2["word_count"] = list
 data2["char_count"] = list_c
+#get rid of all lines that have a traget thats contains morw then one word
 data2 = data2[data2.word_count == 1]
+
+#iterrate over dataframe to get the pos of every traget
+for index, row in data2.iterrows():
+    line = nlp(row.sentence)
+    for token in line:
+        if token.text == row.target:
+            list_pos.append(token.pos_)
+            break
+last_row = data2.iloc[-1]
+
+line = nlp(last_row.sentence)
+for token in line:
+    if token.text == last_row.target:
+        list_pos.append(token.pos_)
+data2["pos"] = list_pos
+print(data2)
 for target in data2["target"]:
     feq = wf(target, "en")
     list_feq.append(feq)
@@ -52,6 +71,18 @@ print(data2["char_count"].corr(data2["probabilistic label"], method="pearson"))
 print("frequency and complexity")
 print(data2["frequency"].corr(data2["probabilistic label"], method="pearson"))
 
-data2.plot.scatter(x = 'char_count', y = 'probabilistic label', s = 100)
-data2.plot.scatter(x = 'frequency', y = 'probabilistic label', s = 100)
+#data2.plot.scatter(x = 'char_count', y = 'probabilistic label', s = 100)
+#data2.plot.scatter(x = 'frequency', y = 'probabilistic label', s = 100)
 
+plt.scatter(x=data2["char_count"], y=data2["probabilistic label"])
+plt.ylabel("complexity")
+plt.xlabel("Length")
+plt.show()
+plt.ylabel("compleyity")
+plt.xlabel("freqeuncy")
+plt.scatter(x=data2["frequency"], y=data2["probabilistic label"])
+plt.show()
+plt.ylabel("compleyity")
+plt.xlabel("POS")
+plt.scatter(x=data2["pos"], y=data2["probabilistic label"])
+plt.show()
