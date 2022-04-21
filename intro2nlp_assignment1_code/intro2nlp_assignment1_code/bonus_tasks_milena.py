5ee8239e600ab0b3b62452b8033c29a9e536c453
milenadeswart <milena.j.deswart@gmail.com>
2022-04-21 12:11:44 +0200

    Start bonus assignment
    - started on bonus assignment
    - improved plots for B7
    - other minor changes

--- intro2nlp_assignment1_code/intro2nlp_assignment1_code/bonus_tasks_milena.py
+++ intro2nlp_assignment1_code/intro2nlp_assignment1_code/bonus_tasks_milena.py
@@ -1,8 +1,9 @@
+### QUESTION 8 ###
 import spacy
 import pandas as pd
 from wordfreq import word_frequency as wf
 import matplotlib.pyplot as plt
-data = pd.read_csv("data/original/english/WikiNews_Train.tsv", sep='\t', usecols=[1,4,9,10], header=None)
+data = pd.read_csv("data/original/spanish/Spanish_Train.tsv", sep='\t', usecols=[1,4,9,10], header=None)
 data.columns = ["sentence","target","binary_label", "probabilistic label"]
 print("binary_label")
 print(data["binary_label"].value_counts())
@@ -19,17 +20,6 @@ print("stdev probabilistic label")
 print(data["probabilistic label"].std())
 
 nlp = spacy.load("en_core_web_sm")
-#counter = 0
-#max = 0
-#for target in data["target"]:
-#    target = nlp(target)
-#    if(len(target.doc)>1):
-#        counter = counter + 1
-#    if(len(target.doc)>max):
-#        max = len(target.doc)
-#        print(target.doc)
-#print(counter)
-#print(max)
 
 #### 8
 data2 = data[data.binary_label == 1]
@@ -44,10 +34,10 @@ for target in data2["target"]:
     list_c.append(len(target.text))
 data2["word_count"] = list
 data2["char_count"] = list_c
-#get rid of all lines that have a traget thats contains morw then one word
+#get rid of all lines that have a target thats contains more than one word
 data2 = data2[data2.word_count == 1]
 
-#iterrate over dataframe to get the pos of every traget
+#iterate over dataframe to get the pos of every traget
 for index, row in data2.iterrows():
     line = nlp(row.sentence)
     for token in line:
@@ -57,9 +47,7 @@ for index, row in data2.iterrows():
 last_row = data2.iloc[-1]
 
 line = nlp(last_row.sentence)
-for token in line:
-    if token.text == last_row.target:
-        list_pos.append(token.pos_)
+list_pos.extend(token.pos_ for token in line if token.text == last_row.target)
 data2["pos"] = list_pos
 print(data2)
 for target in data2["target"]:
@@ -77,12 +65,15 @@ print(data2["frequency"].corr(data2["probabilistic label"], method="pearson"))
 plt.scatter(x=data2["char_count"], y=data2["probabilistic label"])
 plt.ylabel("complexity")
 plt.xlabel("Length")
+plt.title("Scatterplot of word complexity and word length")
 plt.show()
 plt.ylabel("complexity")
 plt.xlabel("frequency")
+plt.title("Scatterplot of word complexity and frequency of word")
 plt.scatter(x=data2["frequency"], y=data2["probabilistic label"])
 plt.show()
 plt.ylabel("complexity")
 plt.xlabel("POS")
+plt.title("Scatterplot of complexity of word and POS label")
 plt.scatter(x=data2["pos"], y=data2["probabilistic label"])
 plt.show()
